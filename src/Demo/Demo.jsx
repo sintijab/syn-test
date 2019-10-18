54a3bc4a25c0add5bc319c08eed399ceb5fe9c37
Sintija <sintija.birgele@accenture.com>
2019-10-18 15:16:55 +0200

    update

--- src/Demo/Demo.jsx
+++ src/Demo/Demo.jsx
@@ -3,6 +3,7 @@ import { getCookie } from '../functions.js';
 import device01 from '../img/device01.png';
 import device02 from '../img/device02.png';
 import device03 from '../img/device03.png';
+import * as gallery from './indexImg.js';
 
 class Demo extends React.Component {
 
@@ -11,17 +12,55 @@ class Demo extends React.Component {
     this.state = {
       loggedIn: !!(getCookie('val')),
       isMobile: !!(window.innerWidth < 479),
+      activeImgSrcFirstSection: 0,
+      activeImgSrcSecondSection: 1,
+      activeImgSrcThirdSection: 2,
+    }
+    this.getDemoSrc = this.getDemoSrc.bind(this);
+  }
+
+  componentDidUpdate() {
+    const { activeImgSrcThirdSection} = this.state;
+    if (activeImgSrcThirdSection < 116) {
+      this.getDemoSrc();
+    } else {
+      this.setState({ activeImgSrcFirstSection: 0, activeImgSrcSecondSection: 1, activeImgSrcThirdSection: 2  });
     }
   }
 
+  getDemoSrc() {
+    const { activeImgSrcFirstSection, activeImgSrcSecondSection, activeImgSrcThirdSection} = this.state;
+    const _this = this;
+
+      setTimeout(() => {
+        _this.setState({
+          activeImgSrcFirstSection: activeImgSrcFirstSection + 3,
+          activeImgSrcSecondSection: activeImgSrcSecondSection + 3,
+          activeImgSrcThirdSection: activeImgSrcThirdSection + 3,
+        })}, 6000);
+  }
+
+  componentDidMount() {
+    this.getDemoSrc();
+  }
+
   render() {
-    const { isMobile, loggedIn } = this.state;
+    const { isMobile, loggedIn, activeImgSrcFirstSection, activeImgSrcSecondSection, activeImgSrcThirdSection } = this.state;
+    let images = gallery;
+
       if (!isMobile && !loggedIn) {
         return (
-          <div className="device-frame">
-            <img src={device01} className="device" alt="bg" />
-            <img src={device02} className="device" alt="bg" />
-            <img src={device03} className="device" alt="bg" />
+          <div>
+            <div className="device-bg">
+              <div className="device-bg-sector"><img src={images.default[activeImgSrcFirstSection]} className="device-img" alt="bg" /></div>
+              <div className="device-bg-sector"><img src={images.default[activeImgSrcSecondSection]} className="device-img" alt="bg" /></div>
+              <div className="device-bg-sector"><img src={images.default[activeImgSrcThirdSection]} className="device-img" alt="bg" /></div>
+            </div>
+            <div className="device-frame">
+              <img src={device01} className="device" alt="bg" />
+              <img src={device02} className="device" alt="bg" />
+              <img src={device03} className="device" alt="bg" />
+           </div>
          </div>
        );
       }
---
+++ src/Demo/indexImg.js
@@ -0,0 +1,238 @@
+import img0 from '../img/demo/0.jpeg';
+import img1 from '../img/demo/1.jpeg';
+import img2 from '../img/demo/2.jpeg';
+import img3 from '../img/demo/3.jpeg';
+import img4 from '../img/demo/4.jpeg';
+import img5 from '../img/demo/5.jpeg';
+import img6 from '../img/demo/6.jpeg';
+import img7 from '../img/demo/7.jpeg';
+import img8 from '../img/demo/8.jpeg';
+import img9 from '../img/demo/9.jpeg';
+import img10 from '../img/demo/10.jpeg';
+import img11 from '../img/demo/11.jpeg';
+import img12 from '../img/demo/12.jpeg';
+import img13 from '../img/demo/13.jpeg';
+import img14 from '../img/demo/14.jpeg';
+import img15 from '../img/demo/15.jpeg';
+import img16 from '../img/demo/16.jpeg';
+import img17 from '../img/demo/17.jpeg';
+import img18 from '../img/demo/18.jpeg';
+import img19 from '../img/demo/19.jpeg';
+import img20 from '../img/demo/20.jpeg';
+import img21 from '../img/demo/21.jpeg';
+import img22 from '../img/demo/22.jpeg';
+import img23 from '../img/demo/23.jpeg';
+import img24 from '../img/demo/24.jpeg';
+import img25 from '../img/demo/25.jpeg';
+import img26 from '../img/demo/26.jpeg';
+import img27 from '../img/demo/27.jpeg';
+import img28 from '../img/demo/28.jpeg';
+import img29 from '../img/demo/29.jpeg';
+import img30 from '../img/demo/30.jpeg';
+import img31 from '../img/demo/31.jpeg';
+import img32 from '../img/demo/32.jpeg';
+import img33 from '../img/demo/33.jpeg';
+import img34 from '../img/demo/34.jpeg';
+import img35 from '../img/demo/35.jpeg';
+import img36 from '../img/demo/36.jpeg';
+import img37 from '../img/demo/37.jpeg';
+import img38 from '../img/demo/38.jpeg';
+import img39 from '../img/demo/39.jpeg';
+import img40 from '../img/demo/40.jpeg';
+import img41 from '../img/demo/41.jpeg';
+import img42 from '../img/demo/42.jpeg';
+import img43 from '../img/demo/43.jpeg';
+import img44 from '../img/demo/44.jpeg';
+import img45 from '../img/demo/45.jpeg';
+import img46 from '../img/demo/46.jpeg';
+import img47 from '../img/demo/47.jpeg';
+import img48 from '../img/demo/48.jpeg';
+import img49 from '../img/demo/49.jpeg';
+import img50 from '../img/demo/50.jpeg';
+import img51 from '../img/demo/51.jpeg';
+import img52 from '../img/demo/52.jpeg';
+import img53 from '../img/demo/53.jpeg';
+import img54 from '../img/demo/54.jpeg';
+import img55 from '../img/demo/55.jpeg';
+import img56 from '../img/demo/56.jpeg';
+import img57 from '../img/demo/57.jpeg';
+import img58 from '../img/demo/58.jpeg';
+import img59 from '../img/demo/59.jpeg';
+import img60 from '../img/demo/60.jpeg';
+import img61 from '../img/demo/61.jpeg';
+import img62 from '../img/demo/62.jpeg';
+import img63 from '../img/demo/63.jpeg';
+import img64 from '../img/demo/64.jpeg';
+import img65 from '../img/demo/65.jpeg';
+import img66 from '../img/demo/66.jpeg';
+import img67 from '../img/demo/67.jpeg';
+import img68 from '../img/demo/68.jpeg';
+import img69 from '../img/demo/69.jpeg';
+import img70 from '../img/demo/70.jpeg';
+import img71 from '../img/demo/71.jpeg';
+import img72 from '../img/demo/72.jpeg';
+import img73 from '../img/demo/73.jpeg';
+import img74 from '../img/demo/74.jpeg';
+import img75 from '../img/demo/75.jpeg';
+import img76 from '../img/demo/76.jpeg';
+import img77 from '../img/demo/77.jpeg';
+import img78 from '../img/demo/78.jpeg';
+import img79 from '../img/demo/79.jpeg';
+import img80 from '../img/demo/80.jpeg';
+import img81 from '../img/demo/81.jpeg';
+import img82 from '../img/demo/82.jpeg';
+import img83 from '../img/demo/83.jpeg';
+import img84 from '../img/demo/84.jpeg';
+import img85 from '../img/demo/85.jpeg';
+import img86 from '../img/demo/86.jpeg';
+import img87 from '../img/demo/87.jpeg';
+import img88 from '../img/demo/88.jpeg';
+import img89 from '../img/demo/89.jpeg';
+import img90 from '../img/demo/90.jpeg';
+import img91 from '../img/demo/91.jpeg';
+import img92 from '../img/demo/92.jpeg';
+import img93 from '../img/demo/93.jpeg';
+import img94 from '../img/demo/94.jpeg';
+import img95 from '../img/demo/95.jpeg';
+import img96 from '../img/demo/96.jpeg';
+import img97 from '../img/demo/97.jpeg';
+import img98 from '../img/demo/98.jpeg';
+import img99 from '../img/demo/99.jpeg';
+import img100 from '../img/demo/100.jpeg';
+import img101 from '../img/demo/101.jpeg';
+import img102 from '../img/demo/102.jpeg';
+import img103 from '../img/demo/103.jpeg';
+import img104 from '../img/demo/104.jpeg';
+import img105 from '../img/demo/105.jpeg';
+import img106 from '../img/demo/106.jpeg';
+import img107 from '../img/demo/107.jpeg';
+import img108 from '../img/demo/108.jpeg';
+import img109 from '../img/demo/109.jpeg';
+import img110 from '../img/demo/110.jpeg';
+import img111 from '../img/demo/111.jpeg';
+import img112 from '../img/demo/112.jpeg';
+import img113 from '../img/demo/113.jpeg';
+import img114 from '../img/demo/114.jpeg';
+import img115 from '../img/demo/115.jpeg';
+import img116 from '../img/demo/116.jpeg';
+
+const gallery = [
+  img0,
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
+  img10,
+  img11,
+  img12,
+  img13,
+  img14,
+  img15,
+  img16,
+  img17,
+  img18,
+  img19,
+  img20,
+  img21,
+  img22,
+  img23,
+  img24,
+  img25,
+  img26,
+  img27,
+  img28,
+  img29,
+  img30,
+  img31,
+  img32,
+  img33,
+  img34,
+  img35,
+  img36,
+  img37,
+  img38,
+  img39,
+  img40,
+  img41,
+  img42,
+  img43,
+  img44,
+  img45,
+  img46,
+  img47,
+  img48,
+  img49,
+  img50,
+  img51,
+  img52,
+  img53,
+  img54,
+  img55,
+  img56,
+  img57,
+  img58,
+  img59,
+  img60,
+  img61,
+  img62,
+  img63,
+  img64,
+  img65,
+  img66,
+  img67,
+  img68,
+  img69,
+  img70,
+  img71,
+  img72,
+  img73,
+  img74,
+  img75,
+  img76,
+  img77,
+  img78,
+  img79,
+  img80,
+  img81,
+  img82,
+  img83,
+  img84,
+  img85,
+  img86,
+  img87,
+  img88,
+  img89,
+  img90,
+  img91,
+  img92,
+  img93,
+  img94,
+  img95,
+  img96,
+  img97,
+  img98,
+  img99,
+  img100,
+  img101,
+  img102,
+  img103,
+  img104,
+  img105,
+  img106,
+  img107,
+  img108,
+  img109,
+  img110,
+  img111,
+  img112,
+  img113,
+  img114,
+  img115,
+  img116
+];
+export default gallery;
